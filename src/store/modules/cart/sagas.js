b17fb0a8b8e02aa0da6087121798ac9e11bee856
Victor Reinor <victorreinor@gmail.com>
2019-08-01 22:06:53 -0300

    adiciona o redirecionamento de rota com o history

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
 
 import {
@@ -38,6 +39,8 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   }
 }
 
